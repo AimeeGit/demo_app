7645aa5d29a9bed9c71058481c88f08a57adc522
AimeeGit <AimeeGit@github.com>
2014-05-20 12:17:15 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = 'd90f968b1a1e72925248cc58aae541b7023eb432569562c5db986fe0b38b11fd0b73e4bc5789349eb4f01370d654c77018121dc94bcb71d74d11ae53d88359c7'
